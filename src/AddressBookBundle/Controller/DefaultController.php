f0cc3681fd87f0ef3bf97582c52dc699d1072db7
tudor <tudor.rusu@hotmail.com>
2020-03-23 11:30:16 +0100

    feat bundle address_book: add AddressBook bundle and shape it with Bootstrap 3.2

---
+++ src/AddressBookBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AddressBookBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('AddressBookBundle:Default:index.html.twig');
+    }
+}
